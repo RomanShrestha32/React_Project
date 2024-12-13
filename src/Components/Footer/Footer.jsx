e077ed13a161aab31ede76ae46de0e0da318e4af
RomanShrestha32 <RomanShrestha32>
2024-12-13 12:51:42 +0545

    University

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className='footer'>
-      <p>@2024 Edusity .All Right Reserved</p>
+      <p>@2024 University .All Right Reserved</p>
       <ul>
         <li>Terms of Services</li>
         <li>Privacy Policy</li>
